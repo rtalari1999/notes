d4f987cf3c2e3cf1a06f6e4fe84004da5363da5e
Raju Talari <rtalari1999@gmail.com>
2024-03-02 00:13:29 +0530

    Template Driven form in angular live class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export class User{
+  name:string;
+  constructor( name:string){
+    this.name = name;
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'notes';
+
+  userDetails = new User('demo user');
+
+  onSubmit(){
+    console.log('______onsubmit', this.userDetails);
+  }
 }
