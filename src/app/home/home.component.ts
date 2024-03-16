86b9c530bbc4d8da6fc253d6eb3862f2013ea25c
Raju Talari <rtalari1999@gmail.com>
2024-03-17 00:13:28 +0530

    cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,17 +2,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [ CommonModule,HttpClientModule],
   providers:[ProductService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
-
+  products: any ;
   constructor(private productService: ProductService){}
 
   ngOnInit(): void {
@@ -22,6 +23,7 @@ export class HomeComponent implements OnInit{
   getProducts():void{
    this.productService.getProductList().subscribe((item)=>{
       console.log('_____Products', item)
+      this.products = item;
     })
   }
 
--- src/app/product.ts
+++ src/app/product.ts
@@ -2,4 +2,6 @@ export interface Product {
 
     pName:string;
     pDescription: string
+    Price: number,
+    pFeatures:{disaplay: string}
 }
